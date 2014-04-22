91beba81472b15f3df5d6bf93f7f7b1b46fd4495
aditya2kx <aditya.2ky@gmail.com>
2014-04-21 23:57:34 -0700

    changes to categorys extractor

--- source/CategoryExtractor.java
+++ source/CategoryExtractor.java
@@ -5,8 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class CategoryExtractor 
 {
@@ -16,24 +14,17 @@ public class CategoryExtractor
 		FileOutputStream fos = null;
 		BufferedReader br = null;
 		String category = null;
-		Pattern p = null;
-		Matcher m;
 		String currentLine = null;
-		boolean foundCategory = false;
 		
 		try 
 		{
-			category = "food";
-			p = Pattern.compile("<"+category+">");
+			category = "Food";
 			fis = new FileInputStream(args[0]);
 			br = new BufferedReader(new InputStreamReader(fis));
 			fos = new FileOutputStream(args[0]+"."+category);
 			while((currentLine = br.readLine()) != null)
 			{
-				foundCategory = false;
-				m = p.matcher(currentLine);
-				foundCategory = m.matches();
-				if(foundCategory)
+				if(currentLine.contains("<"+category+">"))
 				{
 					fos.write((currentLine+"\n").getBytes("UTF8"));
 				}
