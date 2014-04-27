76bf8d3bc693cf1acaf962a339d7617c14358a34
aditya2kx <aditya.2ky@gmail.com>
2014-04-27 07:48:45 -0700

    bug fix in LTC

--- source/LTCGenerator.java
+++ source/LTCGenerator.java
@@ -5,7 +5,16 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
 
 public class LTCGenerator {
 
@@ -16,88 +25,104 @@ public class LTCGenerator {
 	private Set<String> stopWordsSet;
 
 	private List<Map<String, Integer>> documentTermsMap;
-	
+
 	private Map<String, Integer> inverseDocumentFreqMap;
-	
+
 	public LTCGenerator(List<String> sentencesList){
 		documentTermsMap = new ArrayList<>();
 		termToIndexMap = new HashMap<>();
 		inverseDocumentFreqMap = new HashMap<>();
 		stopWordsSet = Utils.getStopWords();
-		
+
 		Set<String> termsList = getUniqueTerms(sentencesList);
 		int count = 0;
 		for(String term : termsList){
-			termToIndexMap.put(term, count);
+			termToIndexMap.put(term, count++);
 		}
-		
-		featureVector = new double[termsList.size()][sentencesList.size()];
+
+		featureVector = new double[sentencesList.size()][termsList.size()];
 	}
 
 	private Set<String> getUniqueTerms(List<String> sentencesList){
 		Set<String> wordSet = new HashSet<>();
-		String[] split;
 		Map<String, Integer> termsMap;
 		int val;
+
+		//load the stanford tokenizer model
+		Properties props = new Properties();
+		props.put("annotators", "tokenize, ssplit");
+		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+		List<CoreMap> sentences;
+		List<CoreLabel> coreLabelList;
+		Annotation document;
 		for(String sentence : sentencesList){
 			termsMap = new HashMap<>();
-			split = sentence.split("\\s+");
-			for(String term : split){
-				if(!stopWordsSet.contains(term) && !term.matches("[^a-zA-Z0-9]+")){
-					val = 0;
-					if(termsMap.containsKey(term)){
-						val = termsMap.get(term);
+			document = new Annotation(sentence);
+			pipeline.annotate(document);
+			sentences = document.get(SentencesAnnotation.class);
+			for(CoreMap sentenceMap : sentences){
+				coreLabelList = sentenceMap.get(TokensAnnotation.class);
+				for(CoreLabel coreLabel : coreLabelList){
+					String term = coreLabel.get(TextAnnotation.class).toLowerCase();
+					if(!stopWordsSet.contains(term) && !term.matches("[^a-zA-Z0-9]+")){
+						val = 0;
+						if(termsMap.containsKey(term)){
+							val = termsMap.get(term);
+						}
+
+						termsMap.put(term, ++val);
+						wordSet.add(term);
 					}
-					
-					termsMap.put(term, ++val);
-					wordSet.add(term);
 				}
 			}
-			
+
 			for(String term : termsMap.keySet()){
 				val = 0;
 				if(inverseDocumentFreqMap.containsKey(term)){
 					val = inverseDocumentFreqMap.get(term);
 				}
-				
+
 				inverseDocumentFreqMap.put(term, ++val);
 			}
-			
+
 			documentTermsMap.add(termsMap);
 		}
-		
+
 		return wordSet;
 	}
-	
+
 	public double[][] calculateTLC(){
 		int tf, isf, termIndex, docIter = 0;
 		double ltcnum = 0.0, ltcdenom;
 		int totalDocs = documentTermsMap.size();
 		Map<String, Double> ltcMap;
+		Set<String> termSet = termToIndexMap.keySet();
 		for(Map<String, Integer> termMap : documentTermsMap){
 			ltcdenom = 0.0;
-			Set<String> termSet = termMap.keySet();
+			//Set<String> termSet = termMap.keySet();
 			ltcMap = new HashMap<>();
-			
+
 			for(String term : termSet){
 				tf = termMap.get(term);
 				isf = inverseDocumentFreqMap.get(term);
-				
+
 				ltcnum = (1.0 + Math.log(tf) * Math.log(totalDocs/isf));
 				ltcdenom += Math.pow(ltcnum, 2.0);
-				
+
 				ltcMap.put(term, ltcnum);
 			}
-			
+
 			//Calculate the feature vector
 			for(String term : termSet){
 				termIndex = termToIndexMap.get(term);
+				ltcnum = ltcMap.get(term);
 				featureVector[docIter][termIndex] = (double) ltcnum/ltcdenom;
 			}
-			
+
 			docIter++;
 		}
-		
+
 		return featureVector;
 	}
 }
