de788ebe6e6f0657e757d7041244d1684f8f1067
santosh-sr <santosh.epzpsg@gmail.com>
2014-04-29 05:46:16 -0700

    add issimilar util method

--- source/Utils.java
+++ source/Utils.java
@@ -7,12 +7,26 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import uk.ac.shef.wit.simmetrics.similaritymetrics.Levenshtein;
+
 public class Utils {
 	private static Set<String> stopWordsSet;
+	
+	private static Set<String> skipWordsSet;
+	
+	private static Levenshtein levenshtein;
 
 	static {
 		try {
+			//Read the stop words
 			readStopWords();
+			
+			//Read the skip words
+			readSkipWords();
+			
+			//Levenshtein
+			levenshtein = new Levenshtein();
+			
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
@@ -23,12 +37,36 @@ public class Utils {
 		try(BufferedReader bufReader = new BufferedReader(new FileReader("stop-words.txt"))){
 			String readLine;
 			while((readLine = bufReader.readLine()) != null){
-				stopWordsSet.add(readLine);
+				stopWordsSet.add(readLine.toLowerCase());
+			}
+		}
+	}
+	
+	private static void readSkipWords() throws FileNotFoundException, IOException{
+		skipWordsSet = new HashSet<>();
+		try(BufferedReader bufReader = new BufferedReader(new FileReader("skip-words.txt"))){
+			String readLine;
+			while((readLine = bufReader.readLine()) != null){
+				skipWordsSet.add(readLine.toLowerCase());
 			}
 		}
 	}
 
 	public static Set<String> getStopWords(){
 		return stopWordsSet;
+	}
+	
+	public static boolean isSimilar(Set<String> keywordSet, String term){
+		for(String keyword : keywordSet){
+			if(Character.toLowerCase(term.charAt(0)) == Character.toLowerCase(keyword.charAt(0)) 
+					&& !skipWordsSet.contains(term.toLowerCase())){
+				float score = levenshtein.getSimilarity(keyword, term);
+				if(score >= 0.8){
+					return true;
+				}
+			}
+		}
+		
+		return false;
 	}
 }
