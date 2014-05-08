6d6d751c38c7a3c6acafdf83ea299d3fa56928f8
aditya2kx <aditya.2ky@gmail.com>
2014-05-07 17:25:24 -0700

    modifed structure for Summary Beans

--- source/SummaryBean.java
+++ source/SummaryBean.java
@@ -1,47 +1,20 @@
 package source;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SummaryBean {
-	private String summary;
+	private List<String> SentenceList;
 	
-	private List<SupportingSentenceBean> supportingSentenceBeanList;
-	
-	public SummaryBean(String summary){
-		this.summary = summary;
-		supportingSentenceBeanList = new LinkedList<>();
+	public SummaryBean(){
+		SentenceList = new ArrayList<String>();
 	}
 	
-	public void addSupportingSentence(int reviewId, String supportingSentence){
-		supportingSentenceBeanList.add(new SupportingSentenceBean(reviewId, supportingSentence));
+	public void addSentence(String sentence){
+		SentenceList.add(sentence);
 	}
 	
-	public List<SupportingSentenceBean> getSupportingSentenceList(){
-		return supportingSentenceBeanList;
-	}
-	
-	public String getSummary(){
-		return summary;
-	}
-	
-	public static class SupportingSentenceBean{
-		private int review_id;
-		
-		private String supportingSentence;
-		
-		
-		public SupportingSentenceBean(int review_id, String supportingSentence){
-			this.review_id = review_id;
-			this.supportingSentence = supportingSentence;
-		}
-		
-		public int getReviewId(){
-			return review_id;
-		}
-		
-		public String getSupportingSentence(){
-			return supportingSentence;
-		}
+	public List<String> getSentenceList(){
+		return SentenceList;
 	}
 }
