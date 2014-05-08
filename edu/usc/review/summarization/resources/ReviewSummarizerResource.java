379558cc5ac26f5f7979bc4c7b14f4d64bb7acc6
santosh-sr <santosh.epzpsg@gmail.com>
2014-05-07 19:04:44 -0700

    change the path in resources file

--- edu/usc/review/summarization/resources/ReviewSummarizerResource.java
+++ edu/usc/review/summarization/resources/ReviewSummarizerResource.java
@@ -1,5 +1,7 @@
 package edu.usc.review.summarization.resources;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 import javax.ws.rs.GET;
@@ -12,6 +14,7 @@ import javax.ws.rs.core.Response.Status;
 import source.BusinessNameMapping;
 import source.CategorySummaryBean;
 import source.HTMLBuilder;
+import source.ReviewsCategoryPhraseExtractor;
 import source.Summarizer;
 
 @Path("/review/summarization")
@@ -20,12 +23,13 @@ public class ReviewSummarizerResource {
 	@Path("/generate")
 	@GET
 	@Produces("plain/txt")
-	public Response createSummary(@QueryParam("name") String businessName) throws UnsupportedEncodingException{
+	public Response createSummary(@QueryParam("name") String businessName) throws FileNotFoundException, IOException{
 		String businessReviewPath = BusinessNameMapping.getBusinessReviewFilePath(businessName);
 		System.out.println(businessName);
 		System.out.println(businessReviewPath);
-		
-		CategorySummaryBean categorySummaryBean = Summarizer.generateSummary(businessReviewPath);
+
+		ReviewsCategoryPhraseExtractor.generatePhrase(businessReviewPath);
+		CategorySummaryBean categorySummaryBean = Summarizer.generateSummary(businessReviewPath + ".phrase.out");
 
 		return Response.status(Status.OK).entity(HTMLBuilder.generateHTMLContent(categorySummaryBean)).build();
 	}
